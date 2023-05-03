030e7a6605588fb2e7adb52c86e2ea0e8a398f95
Demigod-101 <127988520+Demigod-101@users.noreply.github.com>
2023-05-04 04:19:13 +0530

    js class

--- app.js
+++ app.js
@@ -9,52 +9,66 @@ var directionX = 10;
 var directionY = 0;
 var dots = [];
 var highScore = localStorage.getItem("highScore") || 0;
-canvas.width = 400;
-canvas.height = 400;
+const WIDTH = 400;
+const HEIGHT = 400;
+
+class snake{
+  constructor(x,y, dirx, diry, len, arr){
+    this.snakeSegments= arr|| [];
+    this.snakeX= x;
+    this.snakeY= y;
+    this.directionX= dirx;
+    this.directionY= diry;
+    this.snakeLength= len;
+  }
+}
+
+const s= new snake( 0,0,10,0,1,[])
+console.log(s.snakeLength);
 
 document.addEventListener("keydown", changeDirection);
 
 function moveSnake() {
-  snakeSegments.unshift({ x: snakeX, y: snakeY });
-  snakeX += directionX;
-  snakeY += directionY;
-  while (snakeSegments.length > snakeLength) {
-    snakeSegments.pop();
+  s.snakeSegments.unshift({ x: s.snakeX, y: s.snakeY });
+  s.snakeX += s.directionX;
+  s.snakeY += s.directionY;
+  while (s.snakeSegments.length > s.snakeLength) {
+    s.snakeSegments.pop();
   }
 }
 
 function drawSnake() {
-  canvas2d.clearRect(0, 0, canvas.width, canvas.height);
+  canvas2d.clearRect(0, 0, WIDTH, HEIGHT);
   canvas2d.fillStyle = "black";
-  for (let i = 0; i < snakeSegments.length; i++) {
-    canvas2d.fillRect(snakeSegments[i].x, snakeSegments[i].y, 10, 10);
+  for (let i = 0; i < s.snakeSegments.length; i++) {
+    canvas2d.fillRect(s.snakeSegments[i].x, s.snakeSegments[i].y, 10, 10);
   }
 }
 
 function changeDirection(event) {
   var key = event.key;
-  if (key === "ArrowLeft" && directionX != 10) {
-    directionX = -10;
-    directionY = 0;
+  if (key === "ArrowLeft" && s.directionX != 10) {
+    s.directionX = -10;
+    s.directionY = 0;
   }
-  if (key === "ArrowUp" && directionY != 10) {
-    directionX = 0;
-    directionY = -10;
+  if (key === "ArrowUp" && s.directionY != 10) {
+    s.directionX = 0;
+    s.directionY = -10;
   }
-  if (key === "ArrowRight" && directionX != -10) {
-    directionX = 10;
-    directionY = 0;
+  if (key === "ArrowRight" && s.directionX != -10) {
+    s.directionX = 10;
+    s.directionY = 0;
   }
-  if (key === "ArrowDown" && directionY != -10) {
-    directionX = 0;
-    directionY = 10;
+  if (key === "ArrowDown" && s.directionY != -10) {
+    s.directionX = 0;
+    s.directionY = 10;
   }
 }
 
 function spawnDots() {
   if (dots.length < 1) {
-    var dotX = Math.floor((Math.random() * canvas.width) / 10) * 10;
-    var dotY = Math.floor((Math.random() * canvas.height) / 10) * 10;
+    var dotX = Math.floor((Math.random() * WIDTH) / 10) * 10;
+    var dotY = Math.floor((Math.random() * HEIGHT) / 10) * 10;
     dots.push({ x: dotX, y: dotY });
   }
   for (var i = 0; i < dots.length; i++) {
@@ -69,25 +83,25 @@ for (var i = 0; i < dots.length; i++) {
 
 function checkCollision() {
   for (var i = 0; i < dots.length; i++) {
-    if (snakeX == dots[i].x && snakeY == dots[i].y) {
-      snakeLength++;
+    if (s.snakeX == dots[i].x && s.snakeY == dots[i].y) {
+      s.snakeLength++;
       dots.splice(i, 1);
     }
   }
-  let score = snakeLength - 1;
+  let score = s.snakeLength - 1;
   document.getElementById("score").innerHTML = "Score: " + score;
-  for (var i = 0; i < snakeSegments.length; i++) {
+  for (var i = 0; i < s.snakeSegments.length; i++) {
     if (
-      snakeSegments[i].x < -10 ||
-      snakeSegments[i].y < -10 ||
-      snakeSegments[i].x > canvas.width + 10 ||
-      snakeSegments[i].y > canvas.height + 10
+      s.snakeSegments[i].x < -10 ||
+      s.snakeSegments[i].y < -10 ||
+      s.snakeSegments[i].x > WIDTH  ||
+      s.snakeSegments[i].y > HEIGHT 
     ) {
       gameOver();
     }
   }
-  for (var i = 1; i < snakeSegments.length; i++) {
-    if (snakeX === snakeSegments[i].x && snakeY === snakeSegments[i].y) {
+  for (var i = 1; i < s.snakeSegments.length; i++) {
+    if (s.snakeX === s.snakeSegments[i].x && s.snakeY === s.snakeSegments[i].y) {
       gameOver();
     }
   }
@@ -96,24 +110,20 @@ function checkCollision() {
 function gameOver() {
   alert("Game Over!");
   gameEnded = true;
-  snakeSegments = [];
-  snakeX = 0;
-  snakeY = 0;
-  directionX = 0;
-  directionY = 0;
+  s.snakeSegments = [];
+  s.snakeX = 0;
+  s.snakeY = 0;
+  s.directionX = 0;
+  s.directionY = 0;
   location.reload();
 }
 
 function checkHighScore() {
-  // Update high score if necessary
-  if (snakeLength - 1 > highScore) {
-    highScore = snakeLength - 1;
+  if (s.snakeLength - 1 > highScore) {
+    highScore = s.snakeLength - 1;
     localStorage.setItem("highScore", highScore);
   }
-  // // Display high score in the game
   document.getElementById("highScore").innerHTML = "Highscore: " + highScore;
-  // console.log(Math.max(highScore, snakeLength-1));
-  console.log(localStorage.getItem("highScore"));
 }
 
 setInterval(() => {
